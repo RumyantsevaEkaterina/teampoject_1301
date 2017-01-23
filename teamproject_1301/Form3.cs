b2886a65c60b9ea344a63330cfde15fe7855b47d
maki <maki@MAKI->
2017-01-23 21:41:44 +0300

    Исправлено во вкладке "Решение системы уравнений" при возникновении случая деления на "0"

--- teamproject_1301/Form3.cs
+++ teamproject_1301/Form3.cs
@@ -28,6 +28,10 @@ namespace teamproject_1301
             float f = float.Parse(textBox6.Text);
 
            var x = (a * f - c * d) / (a * q - b * d);
+            if ((a * q - b * d) == 0)
+            {
+                MessageBox.Show("Внимание! Деление на 0");
+            }
            var y = (c * q - b * f) / (a * q - b * d);
 
             textBox7.Text = Convert.ToString(x);
