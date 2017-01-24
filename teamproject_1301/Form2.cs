b5b75e838967cfe402e9e740feb63456a1c1353b
maki <maki@MAKI->
2017-01-24 12:58:21 +0300

    В "Решение квадратного уравнения" ввод запятой только один раз

--- teamproject_1301/Form2.cs
+++ teamproject_1301/Form2.cs
@@ -12,9 +12,7 @@ namespace teamproject_1301
 {
     public partial class Form2 : Form
     {
-        private readonly object errorProvider1;
-
-        public Form2()
+               public Form2()
         {
             InitializeComponent();
         }
@@ -45,13 +43,13 @@ namespace teamproject_1301
             float a = float.Parse(textBox2.Text);
             float b = float.Parse(textBox3.Text);
             float c = float.Parse(textBox4.Text);
-
+            
             var d = b * b - 4 * a * c;
            
             if (d<0)
-                {
+            {
                 MessageBox.Show("Дискриминант меньше нуля! Корней нет.");
-                    }
+            }
             if (d == 0)
             {
                 MessageBox.Show("Дискриминант равен 0.");
@@ -75,25 +73,25 @@ namespace teamproject_1301
 
         private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
         {
-            if (!char.IsDigit(e.KeyChar) && e.KeyChar != 8 && e.KeyChar != 44)
+            if (!char.IsDigit(e.KeyChar) && e.KeyChar != 8 && !((e.KeyChar == ',') && (textBox2.Text.IndexOf(",") == -1)))
             {
-                e.Handled = true;
+                if (e.KeyChar != (char)Keys.Back) e.Handled = true;
             }
         }
 
         private void textBox3_KeyPress(object sender, KeyPressEventArgs e)
         {
-            if (!char.IsDigit(e.KeyChar) && e.KeyChar != 8 && e.KeyChar != 44)
+            if (!char.IsDigit(e.KeyChar) && e.KeyChar != 8 && !((e.KeyChar == ',') && (textBox3.Text.IndexOf(",") == -1)))
             {
-                e.Handled = true;
+                if (e.KeyChar != (char)Keys.Back) e.Handled = true;
             }
         }
 
         private void textBox4_KeyPress(object sender, KeyPressEventArgs e)
         {
-            if (!char.IsDigit(e.KeyChar) && e.KeyChar != 8 && e.KeyChar != 44)
+            if (!char.IsDigit(e.KeyChar) && e.KeyChar != 8 && !((e.KeyChar == ',') && (textBox4.Text.IndexOf(",") == -1)))
             {
-                e.Handled = true;
+                if (e.KeyChar != (char)Keys.Back) e.Handled = true;
             }
         }
     }
